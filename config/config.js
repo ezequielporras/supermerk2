e6060d8a9f2bb965d7097364924d1e5250486575
Ger <german.depina@mercadolibre.com>
2019-06-10 12:20:28 -0300

    bumop

--- config/config.js
+++ config/config.js
@@ -2,12 +2,12 @@ const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri: 'mongodb://admin:monster1@ds211029.mlab.com:11029/monsterdb'
-    //   ||
-    // process.env.MONGO_HOST ||
-    // 'mongodb://' + (process.env.IP || 'localhost') + ':' +
-    // (process.env.MONGO_PORT || '27017') +
-    // '/mernproject'
+  mongoUri: 'mongodb+srv://adminDb:integracion123@tpointegracion-whfqh.azure.mongodb.net/test?w=majority' ||
+    process.env.MONGO_HOST ||
+    'mongodb://' + (process.env.IP || 'localhost') + ':' +
+    (process.env.MONGO_PORT || '27017') +
+    '/mernproject'
 }
 
 export default config
+//mongodb+srv://adminDb:integracion123@tpointegracion-whfqh.azure.mongodb.net/test?retryWrites=true&w=majority
